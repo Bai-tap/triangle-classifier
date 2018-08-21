c38291382f7bb9ae03f3ff32522f7eb214d80c31
longhoang94 <longsutls@gmail.com>
2018-08-21 11:36:13 +0700

    add program test Triangle-Classifier

---
+++ src/test/java/TriangleClassifierTest.java
@@ -0,0 +1,44 @@
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+class TriangleClassifierTest {
+    @Test
+    @DisplayName("Test equilateral triangle")
+    public void testEquilateralTriangle() {
+        final int A = 2;
+        final int B = 2;
+        final int C = 2;
+
+        String expected = "equilateral triangle";
+        String actual = TriangleClassifier.result(A, B, C);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Test isosceles triangle")
+    public void testIsoscelesTriangle() {
+        final int A = 2;
+        final int B = 2;
+        final int C = 3;
+
+        String expected = "isosceles triangle";
+        String actual = TriangleClassifier.result(A, B, C);
+        assertEquals(expected, actual);
+    }
+    @Test
+    @DisplayName("Test normal triangle")
+    public void testNormalTriangle() {
+        final int A = 3;
+        final int B = 4;
+        final int C = 5;
+
+        String expected = "normal triangle";
+        String actual = TriangleClassifier.result(A, B, C);
+        assertEquals(expected, actual);
+    }
+
+}
