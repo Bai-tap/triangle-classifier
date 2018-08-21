499b882ab36e18b67e744a9b38470ef8037849ac
longhoang94 <longsutls@gmail.com>
2018-08-21 12:38:46 +0700

    add program Triangle-Classifier

--- src/main/java/TriangleClassifier.java
+++ src/main/java/TriangleClassifier.java
@@ -5,7 +5,7 @@ public class TriangleClassifier {
     public static String result(int a, int b, int c) {
         if (a == b && a == c) return "equilateral triangle";
         else if (a == b && a != c) return "isosceles triangle";
-        else if (a != b && a != c && a + b > c) return "normal triangle";
-        return "";
+        else if (a != b && a != c && a + b > c && b + c > a && a + c > b) return "normal triangle";
+        return "not a triangle";
     }
 }
